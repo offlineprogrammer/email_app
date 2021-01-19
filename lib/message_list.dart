63943b07b6b057db9a06f5adbb9790cf70e3c6f6
offlineProgrammer <mmalaka@gmail.com>
2021-01-19 15:28:34 -0800

    Navigating

--- lib/message_list.dart
+++ lib/message_list.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 
+import 'package:email_app/compose_button.dart';
 import 'package:email_app/message.dart';
+import 'package:email_app/message_compose.dart';
+import 'package:email_app/message_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
@@ -25,55 +28,64 @@ class _MessageListState extends State<MessageList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: () {
-                var _messages = Message.browse();
-                setState(() {
-                  messages = _messages;
-                });
-              },
-            )
-          ],
-        ),
-        body: FutureBuilder(
-          future: messages,
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-              case ConnectionState.active:
-                return Center(child: CircularProgressIndicator());
-              case ConnectionState.done:
-                if (snapshot.hasError)
-                  return Text('There was an error : ${snapshot.error}');
-                var messages = snapshot.data;
-                return ListView.separated(
-                  itemCount: messages.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    Message message = messages[index];
-                    return ListTile(
-                      title: Text(message.subject),
-                      isThreeLine: true,
-                      leading: CircleAvatar(
-                        child: Text('PJ'),
-                      ),
-                      subtitle: Text(
-                        message.body,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return Divider();
-                  },
-                );
-            }
-          },
-        ));
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              var _messages = Message.browse();
+              setState(() {
+                messages = _messages;
+              });
+            },
+          )
+        ],
+      ),
+      body: FutureBuilder(
+        future: messages,
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.done:
+              if (snapshot.hasError)
+                return Text('There was an error : ${snapshot.error}');
+              var messages = snapshot.data;
+              return ListView.separated(
+                itemCount: messages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Message message = messages[index];
+                  return ListTile(
+                    title: Text(message.subject),
+                    isThreeLine: true,
+                    leading: CircleAvatar(
+                      child: Text('PJ'),
+                    ),
+                    subtitle: Text(
+                      message.body,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => MessageDetail(
+                                  message.subject, message.body)));
+                    },
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return Divider();
+                },
+              );
+          }
+        },
+      ),
+      floatingActionButton: ComposeButton(),
+    );
   }
 }
