656bda2b3ebd15e2ee64c27a1491fe57aeb265a8
offlineProgrammer <mmalaka@gmail.com>
2021-01-19 14:14:22 -0800

    Future Builder

--- lib/message.dart
+++ lib/message.dart
@@ -1,10 +1,31 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+part 'message.g.dart';
+
+@JsonSerializable()
 class Message {
   final String subject;
   final String body;
 
   Message(this.subject, this.body);
 
-  Message.fromJson(Map<String, dynamic> json)
-      : subject = json['subject'],
-        body = json['body'];
+  factory Message.fromJson(Map<String, dynamic> json) =>
+      _$MessageFromJson(json);
+
+  static Future<List<Message>> browse() async {
+    List collection;
+    List<Message> _messages;
+    var url = 'https://run.mocky.io/v3/72602836-4b7c-435c-97d2-ace363ece212';
+
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      collection = convert.jsonDecode(response.body);
+      _messages = collection.map((json) => Message.fromJson(json)).toList();
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+    return _messages;
+  }
 }
--- lib/message_list.dart
+++ lib/message_list.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 
 import 'package:email_app/message.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class MessageList extends StatefulWidget {
   final String title;
@@ -14,50 +15,65 @@ class MessageList extends StatefulWidget {
 }
 
 class _MessageListState extends State<MessageList> {
-  var messages = const [];
-
-  Future loadMessageList() async {
-    var content = await rootBundle.loadString('data/message.json');
-    List collection = json.decode(content);
-    List<Message> _messages =
-        collection.map((json) => Message.fromJson(json)).toList();
-    setState(() {
-      messages = _messages;
-    });
-  }
+  Future<List<Message>> messages;
 
   void initState() {
-    loadMessageList();
+    messages = Message.browse();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: ListView.separated(
-        itemCount: messages.length,
-        itemBuilder: (BuildContext context, int index) {
-          Message message = messages[index];
-          return ListTile(
-            title: Text(message.subject),
-            isThreeLine: true,
-            leading: CircleAvatar(
-              child: Text('PJ'),
-            ),
-            subtitle: Text(
-              message.body,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return Divider();
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                var _messages = Message.browse();
+                setState(() {
+                  messages = _messages;
+                });
+              },
+            )
+          ],
+        ),
+        body: FutureBuilder(
+          future: messages,
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+              case ConnectionState.active:
+                return Center(child: CircularProgressIndicator());
+              case ConnectionState.done:
+                if (snapshot.hasError)
+                  return Text('There was an error : ${snapshot.error}');
+                var messages = snapshot.data;
+                return ListView.separated(
+                  itemCount: messages.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    Message message = messages[index];
+                    return ListTile(
+                      title: Text(message.subject),
+                      isThreeLine: true,
+                      leading: CircleAvatar(
+                        child: Text('PJ'),
+                      ),
+                      subtitle: Text(
+                        message.body,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return Divider();
+                  },
+                );
+            }
+          },
+        ));
   }
 }
